0507ea4a01a60762964f42154d4bca39c14901d1
Waldenilson Carneiro <wal.denilson@hotmail.com>
2015-04-13 15:25:28 -0300

    adicao do app odslib no projeto.

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project.core',
+    'project.odslib',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
